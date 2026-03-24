6f8d7ec75ee4fd978c02efb206462232b3416a61
Gonzalo <gdiazfernandez@safareyes.es>
2026-03-24 01:07:20 +0100

    errores solucionados ConexionDB.java

--- src/main/java/esports/dao/ConexionDB.java
+++ src/main/java/esports/dao/ConexionDB.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    // La ruta exacta a tu base de datos
+
     private static final String URL = "jdbc:mariadb://localhost:3306/esports";
-    // El usuario por defecto de XAMPP
+
     private static final String USUARIO = "root";
-    // Contraseña vacía por defecto en XAMPP
+
     private static final String CONTRASENA = "1234";
 
     public static Connection conectar() throws SQLException {
-        // Aquí le obligamos a usar el usuario "root" siempre
+
         return DriverManager.getConnection(URL, USUARIO, CONTRASENA);
     }
 }
